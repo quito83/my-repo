ef90c65806a34d56e435384be0087ef36c6f4d57
Quito <quitoapps@gmail.com>
2022-08-18 20:45:29 +0000

    create first Component

---
+++ my-app/src/components/Testimonios.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Testimonios() {
+  return (
+    <div className="contenedor-testimonio">
+      <p> Hola </p>
+      <p> Hola 2</p>
+    </div>
+  )
+}
+
+export default Testimonios;
